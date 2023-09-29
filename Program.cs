f8b7361313827388355c046eeec5a954e34d7930
Julii Abadjiev <117214264+Jul1ius@users.noreply.github.com>
2023-09-29 16:16:18 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _1._Number_Type
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            if (num < 0)
+                Console.WriteLine("negative");
+            else if (num > 0)
+                Console.WriteLine("positive");
+            else if (num == 0)
+                Console.WriteLine("zero");
+        }
+    }
+}
